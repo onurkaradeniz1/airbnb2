9c24071fac37b990d5e4d573a62ab0b803c554f6
Onur Karadeniz <onurkaradeniz1@gmail.com>
2022-01-21 09:49:10 +0100

    not completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating flats"
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Grand Canyon',
+  address: 'Colorado USA',
+  description: 'Amazing place',
+  price_per_night: 100,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Topkapi',
+  address: 'Istanbul',
+  description: 'Great History',
+  price_per_night: 150,
+  number_of_guests: 5
+)
+
+
+Flat.create!(
+  name: 'Niagara',
+  address: 'Canada',
+  description: 'Great View',
+  price_per_night: 200,
+  number_of_guests: 4
+)
+
+puts "done"
